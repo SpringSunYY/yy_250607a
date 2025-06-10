466d5fb92a91401faa968f973a7fb7697c641437
SpringSunYY <3217778348@qq.com>
2025-06-10 22:01:58 +0800

    供应商信息新增修改

--- store-ui/src/views/manage/supplierInfo/index.vue
+++ store-ui/src/views/manage/supplierInfo/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app-container">
     <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="68px">
-      <el-form-item label="供应商编号" prop="supplierId">
+      <el-form-item label="编号" prop="supplierId">
         <el-input
           v-model="queryParams.supplierId"
           placeholder="请输入供应商编号"
@@ -9,7 +9,7 @@
           @keyup.enter.native="handleQuery"
         />
       </el-form-item>
-      <el-form-item label="供应商名称" prop="supplierName">
+      <el-form-item label="名称" prop="supplierName">
         <el-input
           v-model="queryParams.supplierName"
           placeholder="请输入供应商名称"
@@ -86,7 +86,8 @@
           size="mini"
           @click="handleAdd"
           v-hasPermi="['manage:supplierInfo:add']"
-        >新增</el-button>
+        >新增
+        </el-button>
       </el-col>
       <el-col :span="1.5">
         <el-button
@@ -97,7 +98,8 @@
           :disabled="single"
           @click="handleUpdate"
           v-hasPermi="['manage:supplierInfo:edit']"
-        >修改</el-button>
+        >修改
+        </el-button>
       </el-col>
       <el-col :span="1.5">
         <el-button
@@ -108,7 +110,8 @@
           :disabled="multiple"
           @click="handleDelete"
           v-hasPermi="['manage:supplierInfo:remove']"
-        >删除</el-button>
+        >删除
+        </el-button>
       </el-col>
       <el-col :span="1.5">
         <el-button
@@ -118,36 +121,49 @@
           size="mini"
           @click="handleExport"
           v-hasPermi="['manage:supplierInfo:export']"
-        >导出</el-button>
+        >导出
+        </el-button>
       </el-col>
       <right-toolbar :showSearch.sync="showSearch" @queryTable="getList" :columns="columns"></right-toolbar>
     </el-row>
 
     <el-table v-loading="loading" :data="supplierInfoList" @selection-change="handleSelectionChange">
-      <el-table-column type="selection" width="55" align="center" />
-      <el-table-column label="供应商编号" align="center" v-if="columns[0].visible" prop="supplierId" />
-        <el-table-column label="供应商名称" :show-overflow-tooltip="true" align="center" v-if="columns[1].visible" prop="supplierName" />
-        <el-table-column label="手机号码" :show-overflow-tooltip="true" align="center" v-if="columns[2].visible" prop="phone" />
-        <el-table-column label="地址" :show-overflow-tooltip="true" align="center" v-if="columns[3].visible" prop="address" />
-        <el-table-column label="图片" align="center" v-if="columns[4].visible" prop="supplierImage" width="100">
+      <el-table-column type="selection" width="55" align="center"/>
+      <el-table-column label="供应商编号" align="center" v-if="columns[0].visible" prop="supplierId"/>
+      <el-table-column label="供应商名称" :show-overflow-tooltip="true" align="center" v-if="columns[1].visible"
+                       prop="supplierName"
+      />
+      <el-table-column label="手机号码" :show-overflow-tooltip="true" align="center" v-if="columns[2].visible"
+                       prop="phone"
+      />
+      <el-table-column label="地址" :show-overflow-tooltip="true" align="center" v-if="columns[3].visible"
+                       prop="address"
+      />
+      <el-table-column label="图片" align="center" v-if="columns[4].visible" prop="supplierImage" width="100">
         <template slot-scope="scope">
           <image-preview :src="scope.row.supplierImage" :width="50" :height="50"/>
         </template>
       </el-table-column>
-        <el-table-column label="创建人" :show-overflow-tooltip="true" align="center" v-if="columns[5].visible" prop="createBy" />
-        <el-table-column label="创建时间" align="center" v-if="columns[6].visible" prop="createTime" width="180">
+      <el-table-column label="创建人" :show-overflow-tooltip="true" align="center" v-if="columns[5].visible"
+                       prop="createBy"
+      />
+      <el-table-column label="创建时间" align="center" v-if="columns[6].visible" prop="createTime" width="180">
         <template slot-scope="scope">
           <span>{{ parseTime(scope.row.createTime, '{y}-{m}-{d}') }}</span>
         </template>
       </el-table-column>
-        <el-table-column label="更新人" :show-overflow-tooltip="true" align="center" v-if="columns[7].visible" prop="updateBy" />
-        <el-table-column label="更新时间" align="center" v-if="columns[8].visible" prop="updateTime" width="180">
+      <el-table-column label="更新人" :show-overflow-tooltip="true" align="center" v-if="columns[7].visible"
+                       prop="updateBy"
+      />
+      <el-table-column label="更新时间" align="center" v-if="columns[8].visible" prop="updateTime" width="180">
         <template slot-scope="scope">
           <span>{{ parseTime(scope.row.updateTime, '{y}-{m}-{d}') }}</span>
         </template>
       </el-table-column>
-        <el-table-column label="备注" :show-overflow-tooltip="true" align="center" v-if="columns[9].visible" prop="remark" />
-        <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+      <el-table-column label="备注" :show-overflow-tooltip="true" align="center" v-if="columns[9].visible"
+                       prop="remark"
+      />
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
         <template slot-scope="scope">
           <el-button
             size="mini"
@@ -155,14 +171,16 @@
             icon="el-icon-edit"
             @click="handleUpdate(scope.row)"
             v-hasPermi="['manage:supplierInfo:edit']"
-          >修改</el-button>
+          >修改
+          </el-button>
           <el-button
             size="mini"
             type="text"
             icon="el-icon-delete"
             @click="handleDelete(scope.row)"
             v-hasPermi="['manage:supplierInfo:remove']"
-          >删除</el-button>
+          >删除
+          </el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -177,21 +195,21 @@
 
     <!-- 添加或修改供货商信息对话框 -->
     <el-dialog :title="title" :visible.sync="open" width="500px" append-to-body>
-      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+      <el-form ref="form" :model="form" :rules="rules" label-width="100px">
         <el-form-item label="供应商名称" prop="supplierName">
-          <el-input v-model="form.supplierName" placeholder="请输入供应商名称" />
+          <el-input v-model="form.supplierName" placeholder="请输入供应商名称"/>
         </el-form-item>
         <el-form-item label="手机号码" prop="phone">
-          <el-input v-model="form.phone" placeholder="请输入手机号码" />
+          <el-input v-model="form.phone" placeholder="请输入手机号码"/>
         </el-form-item>
         <el-form-item label="地址" prop="address">
-          <el-input v-model="form.address" placeholder="请输入地址" />
+          <el-input v-model="form.address" placeholder="请输入地址"/>
         </el-form-item>
         <el-form-item label="图片" prop="supplierImage">
           <image-upload v-model="form.supplierImage"/>
         </el-form-item>
         <el-form-item label="备注" prop="remark">
-          <el-input v-model="form.remark" type="textarea" placeholder="请输入内容" />
+          <el-input v-model="form.remark" type="textarea" placeholder="请输入内容"/>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
@@ -203,25 +221,31 @@
 </template>
 
 <script>
-import { listSupplierInfo, getSupplierInfo, delSupplierInfo, addSupplierInfo, updateSupplierInfo } from "@/api/manage/supplierInfo";
+import {
+  listSupplierInfo,
+  getSupplierInfo,
+  delSupplierInfo,
+  addSupplierInfo,
+  updateSupplierInfo
+} from '@/api/manage/supplierInfo'
 
 export default {
-  name: "SupplierInfo",
+  name: 'SupplierInfo',
   data() {
     return {
       //表格展示列
       columns: [
-        { key: 0, label: '供应商编号', visible: true },
-          { key: 1, label: '供应商名称', visible: true },
-          { key: 2, label: '手机号码', visible: true },
-          { key: 3, label: '地址', visible: true },
-          { key: 4, label: '图片', visible: true },
-          { key: 5, label: '创建人', visible: true },
-          { key: 6, label: '创建时间', visible: true },
-          { key: 7, label: '更新人', visible: true },
-          { key: 8, label: '更新时间', visible: true },
-          { key: 9, label: '备注', visible: true },
-        ],
+        { key: 0, label: '供应商编号', visible: false },
+        { key: 1, label: '供应商名称', visible: true },
+        { key: 2, label: '手机号码', visible: true },
+        { key: 3, label: '地址', visible: true },
+        { key: 4, label: '图片', visible: true },
+        { key: 5, label: '创建人', visible: true },
+        { key: 6, label: '创建时间', visible: true },
+        { key: 7, label: '更新人', visible: false },
+        { key: 8, label: '更新时间', visible: false },
+        { key: 9, label: '备注', visible: false }
+      ],
       // 遮罩层
       loading: true,
       // 选中数组
@@ -237,7 +261,7 @@ export default {
       // 供货商信息表格数据
       supplierInfoList: [],
       // 弹出层标题
-      title: "",
+      title: '',
       // 是否显示弹出层
       open: false,
       // 备注时间范围
@@ -255,56 +279,61 @@ export default {
         createBy: null,
         createTime: null,
         updateBy: null,
-        updateTime: null,
+        updateTime: null
       },
       // 表单参数
       form: {},
       // 表单校验
       rules: {
         supplierName: [
-          { required: true, message: "供应商名称不能为空", trigger: "blur" }
+          { required: true, message: '供应商名称不能为空', trigger: 'blur' }
         ],
         phone: [
-          { required: true, message: "手机号码不能为空", trigger: "blur" }
+          { required: true, message: '手机号码不能为空', trigger: 'blur' },
+          {
+            pattern: /^1[3|4|5|6|7|8|9][0-9]\d{8}$/,
+            message: '请输入正确的手机号码',
+            trigger: 'blur'
+          }
         ],
         address: [
-          { required: true, message: "地址不能为空", trigger: "blur" }
+          { required: true, message: '地址不能为空', trigger: 'blur' }
         ],
         createBy: [
-          { required: true, message: "创建人不能为空", trigger: "blur" }
+          { required: true, message: '创建人不能为空', trigger: 'blur' }
         ],
         createTime: [
-          { required: true, message: "创建时间不能为空", trigger: "blur" }
-        ],
+          { required: true, message: '创建时间不能为空', trigger: 'blur' }
+        ]
       }
-    };
+    }
   },
   created() {
-    this.getList();
+    this.getList()
   },
   methods: {
     /** 查询供货商信息列表 */
     getList() {
-      this.loading = true;
-      this.queryParams.params = {};
+      this.loading = true
+      this.queryParams.params = {}
       if (null != this.daterangeCreateTime && '' != this.daterangeCreateTime) {
-        this.queryParams.params["beginCreateTime"] = this.daterangeCreateTime[0];
-        this.queryParams.params["endCreateTime"] = this.daterangeCreateTime[1];
+        this.queryParams.params['beginCreateTime'] = this.daterangeCreateTime[0]
+        this.queryParams.params['endCreateTime'] = this.daterangeCreateTime[1]
       }
       if (null != this.daterangeUpdateTime && '' != this.daterangeUpdateTime) {
-        this.queryParams.params["beginUpdateTime"] = this.daterangeUpdateTime[0];
-        this.queryParams.params["endUpdateTime"] = this.daterangeUpdateTime[1];
+        this.queryParams.params['beginUpdateTime'] = this.daterangeUpdateTime[0]
+        this.queryParams.params['endUpdateTime'] = this.daterangeUpdateTime[1]
       }
       listSupplierInfo(this.queryParams).then(response => {
-        this.supplierInfoList = response.rows;
-        this.total = response.total;
-        this.loading = false;
-      });
+        this.supplierInfoList = response.rows
+        this.total = response.total
+        this.loading = false
+      })
     },
     // 取消按钮
     cancel() {
-      this.open = false;
-      this.reset();
+      this.open = false
+      this.reset()
     },
     // 表单重置
     reset() {
@@ -319,72 +348,73 @@ export default {
         updateBy: null,
         updateTime: null,
         remark: null
-      };
-      this.resetForm("form");
+      }
+      this.resetForm('form')
     },
     /** 搜索按钮操作 */
     handleQuery() {
-      this.queryParams.pageNum = 1;
-      this.getList();
+      this.queryParams.pageNum = 1
+      this.getList()
     },
     /** 重置按钮操作 */
     resetQuery() {
-      this.daterangeCreateTime = [];
-      this.daterangeUpdateTime = [];
-      this.resetForm("queryForm");
-      this.handleQuery();
+      this.daterangeCreateTime = []
+      this.daterangeUpdateTime = []
+      this.resetForm('queryForm')
+      this.handleQuery()
     },
     // 多选框选中数据
     handleSelectionChange(selection) {
       this.ids = selection.map(item => item.supplierId)
-      this.single = selection.length!==1
+      this.single = selection.length !== 1
       this.multiple = !selection.length
     },
     /** 新增按钮操作 */
     handleAdd() {
-      this.reset();
-      this.open = true;
-      this.title = "添加供货商信息";
+      this.reset()
+      this.open = true
+      this.title = '添加供货商信息'
     },
     /** 修改按钮操作 */
     handleUpdate(row) {
-      this.reset();
+      this.reset()
       const supplierId = row.supplierId || this.ids
       getSupplierInfo(supplierId).then(response => {
-        this.form = response.data;
-        this.open = true;
-        this.title = "修改供货商信息";
-      });
+        this.form = response.data
+        this.open = true
+        this.title = '修改供货商信息'
+      })
     },
     /** 提交按钮 */
     submitForm() {
-      this.$refs["form"].validate(valid => {
+      this.$refs['form'].validate(valid => {
         if (valid) {
           if (this.form.supplierId != null) {
             updateSupplierInfo(this.form).then(response => {
-              this.$modal.msgSuccess("修改成功");
-              this.open = false;
-              this.getList();
-            });
+              this.$modal.msgSuccess('修改成功')
+              this.open = false
+              this.getList()
+            })
           } else {
             addSupplierInfo(this.form).then(response => {
-              this.$modal.msgSuccess("新增成功");
-              this.open = false;
-              this.getList();
-            });
+              this.$modal.msgSuccess('新增成功')
+              this.open = false
+              this.getList()
+            })
           }
         }
-      });
+      })
     },
     /** 删除按钮操作 */
     handleDelete(row) {
-      const supplierIds = row.supplierId || this.ids;
+      const supplierIds = row.supplierId || this.ids
       this.$modal.confirm('是否确认删除供货商信息编号为"' + supplierIds + '"的数据项？').then(function() {
-        return delSupplierInfo(supplierIds);
+        return delSupplierInfo(supplierIds)
       }).then(() => {
-        this.getList();
-        this.$modal.msgSuccess("删除成功");
-      }).catch(() => {});
+        this.getList()
+        this.$modal.msgSuccess('删除成功')
+      }).catch(() => {
+      })
     },
     /** 导出按钮操作 */
     handleExport() {
@@ -393,5 +423,5 @@ export default {
       }, `supplierInfo_${new Date().getTime()}.xlsx`)
     }
   }
-};
+}
 </script>
